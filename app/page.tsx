c391e17d4b9656b1e9f64f4ac8ab8dbbfa0f0887
shashwa7-dev <shashwa7.dev@gmail.com>
2025-11-19 21:41:11 +0530

    create new wf card added

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 // Import hero section components
 import WorkflowBuilder from "@/components/app/(home)/sections/workflow-builder/WorkflowBuilder";
 import { useWorflows } from "@/lib/workflow/hooks/useGetAllWFs";
-import { Loader2 } from "lucide-react";
+import { BadgePlus, Loader2 } from "lucide-react";
 
 function StyleGuidePageContent() {
   const router = useRouter();
@@ -19,20 +19,33 @@ function StyleGuidePageContent() {
   };
 
   return (
-    <div className="border min-h-screen flex items-center justify-center">
-      <div className="max-w-xl w-full">
-        <h3 className="text-lg border-b pb-4 font-medium text-accent-black mb-10">
+    <div className="min-h-screen flex">
+      <div className="max-w-4xl w-full relative flex-1  mx-auto py-12">
+        <h3 className="text-lg border-b p-4 font-medium text-accent-black mb-10 sticky top-0 bg-background-base">
           Your Workflows
         </h3>
         {loading_workflows ? (
-          <div className="flex items-center justify-center py-20 gap-2 text-label-medium min-h-[30dvh] w-full border rounded-xl">
-            <Loader2 className="flex-shrink-0 w-15 h-15 animate-spin text-heat-100" />
-            <p>Loading Workflows ...</p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
+            <WorkflowItemSkeleton />
+            <WorkflowItemSkeleton /> <WorkflowItemSkeleton />
+            <WorkflowItemSkeleton /> <WorkflowItemSkeleton />
+            <WorkflowItemSkeleton />
           </div>
         ) : workflows_data?.data.length === 0 ? (
           <p className="text-xs text-black-alpha-48">No workflow available.</p>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
+            <button
+              onClick={() => {
+                router.push(`/workflows`);
+              }}
+              className={`p-16 rounded-12 border transition-all text-left hover:border-heat-100 hover:shadow-sm border-border-faint`}
+            >
+              <div className="mb-5 flex flex-col items-center justify-center gap-2">
+                <BadgePlus className="w-20 h-20" />
+                <p className="text-sm">Create New Workflow</p>
+              </div>
+            </button>
             {workflows_data?.data.map((item) => (
               <button
                 key={item.id}
@@ -56,7 +69,7 @@ function StyleGuidePageContent() {
               </button>
             ))}
           </div>
-        )}{" "}
+        )}
       </div>
     </div>
   );
@@ -67,5 +80,24 @@ export default function StyleGuidePage() {
     <Suspense fallback={<div>Loading...</div>}>
       <StyleGuidePageContent />
     </Suspense>
+  );
+}
+
+export function WorkflowItemSkeleton() {
+  return (
+    <div
+      className={`p-16 rounded-12 border border-border-faint bg-black-alpha-4 animate-pulse`}
+    >
+      <div className="space-y-1 mb-5">
+        <div className="h-10 w-24 bg-black-alpha-12 rounded" />
+        <div className="h-10 w-32 bg-black-alpha-8 rounded" />
+      </div>
+
+      <div className="space-y-2">
+        <div className="h-10 w-full bg-black-alpha-8 rounded" />
+        <div className="h-10 w-5/6 bg-black-alpha-8 rounded" />
+        <div className="h-10 w-4/6 bg-black-alpha-8 rounded" />
+      </div>
+    </div>
   );
 }
